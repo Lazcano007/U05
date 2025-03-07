22be01c4e1137caa26f54ff58e7fa4b01719ce39
Lazcano007 <Pedrolazcano@live.com>
2025-03-07 17:42:48 +0100

    Fixade kopplingen till server och databasen

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,16 @@
-console.log("dsd");
-console.log("dsdfsfsdfd");
+import express, {Express, Request, Response } from "express";
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 3636;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hello world");
+});
+
+app.listen(PORT,()=> {
+    console.log(`Program is running on http://localhost:${PORT}`);
+})

470fccfd09b04494409c1fee495aa514639dfad2
Lazcano007 <Pedrolazcano@live.com>
2025-03-12 18:22:19 +0100

    fixed space between code lines

--- Interface/users.ts
+++ Interface/users.ts
@@ -1,7 +1,5 @@
 import { Document } from "mongoose";  // {Document} används för att definera strukturen för dokumnetet i DB
 
-
-
 // definerar strukturen för dokumentet i databasen
 export interface UserData{
     id: number;

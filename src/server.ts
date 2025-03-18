54b58f31924d1841a9a2a562f7015730acb38530
Lazcano007 <Pedrolazcano@live.com>
2025-03-18 15:22:54 +0100

    Changed welcome text

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world");
+    res.send("Wag1 G...Welcome to my pets-API :)");
 });
 
 app.use("/api/Users", userRouter);

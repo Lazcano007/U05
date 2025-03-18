9b92722c8f921902e8df73afc73bca34fa223d3e
Lazcano007 <Pedrolazcano@live.com>
2025-03-18 14:19:10 +0100

    Rewrote connection message

--- database/db.ts
+++ database/db.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
+
 async function connectDB() {
     try{
         const URL = process.env.MONGO_URL || "";
         await mongoose.connect(URL);
-        console.log("Database connected successfully");
+        console.log(`Database connected successfully ${URL}`);
     }catch(error:unknown){
         if (error instanceof Error) {
             console.log(`Theres been an error while connecting to DB:${error.message}`);

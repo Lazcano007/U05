754f5157bfb24c96b7b0a23b7db5d34cfc392edb
Lazcano007 <Pedrolazcano@live.com>
2025-03-18 14:10:58 +0100

    Added routes for each controller and cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import express, {Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDB from "../database/db";
-import { getPets } from "../Controller/petController";
-import { getUser } from "../Controller/userController";
-import router from "../Routes/userRouter";
+import userRouter from "../Routes/userRouter";
+import petRouter from "../Routes/petRouter";
+import vaccinRouter from "../Routes/vaccinRouter";
+import journalRouter from "../Routes/journalRouter";
+import appoinmentRouter from "../Routes/appointmentRouter";
+
+
 
 dotenv.config();
 connectDB();
 
+const cors = require("cors");
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 3636;
+
+app.use(cors(
+    {
+    origin: "http://localhost:3100",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    }
+));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,10 +30,11 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello world");
 });
 
-
-app.use("/api/users", router);
-app.use("/Pets", getPets);
-app.use("/Users", getUser);
+app.use("/api/Users", userRouter);
+app.use("/api/Pets", petRouter);
+app.use("/api/Vaccins", vaccinRouter);
+app.use("/api/Journals", journalRouter);
+app.use("/api/Appointments", appoinmentRouter);
 
 
 

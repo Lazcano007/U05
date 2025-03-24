a1abe00e999d74d5f35e719ba7c6333031f1310e
Lazcano007 <Pedrolazcano@live.com>
2025-03-24 12:41:21 +0100

    Implemanted auth for logout

--- Routes/userRouter.ts
+++ Routes/userRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import authenticateToken from "../Middlewear/auth";    //importing middlewear
 import { createUser, getUsers, loginUser, logoutUser} from "../Controller/userController"; // Kontrollera denna sökväg!
 const userRouter = express.Router();
 
 userRouter.post("/registration", createUser);
 userRouter.post("/login", loginUser);
+
 userRouter.get("/", getUsers);
-userRouter.post("/logout", logoutUser);
+userRouter.post("/logout", authenticateToken, logoutUser);
 export default userRouter;

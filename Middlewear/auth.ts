24d1af2d51ed7ddbd00734396ccd115047ca176c
Lazcano007 <Pedrolazcano@live.com>
2025-03-25 13:35:13 +0100

    Exported Userequest from auth

--- Middlewear/auth.ts
+++ Middlewear/auth.ts
@@ -3,7 +3,7 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { UserData, userModel } from "../Model/userModel"; 
 
 
-interface UserRequest extends Request {
+export interface UserRequest extends Request {
   user?: UserData;
 }
 

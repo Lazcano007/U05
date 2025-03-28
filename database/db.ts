964681f112d0cf1922e4c1198eca084a3ae1b8d5
Lazcano007 <Pedrolazcano@live.com>
2025-03-28 16:20:02 +0100

    Formated DB connection file

--- database/db.ts
+++ database/db.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-
 async function connectDB() {
-    try{
-        const URL = process.env.MONGO_URL || "";
-        await mongoose.connect(URL);
-        console.log(`Database connected successfully ${URL}`);
-    }catch(error:unknown){
-        if (error instanceof Error) {
-            console.log(`Theres been an error while connecting to DB:${error.message}`);
-        }
+  try {
+    const URL = process.env.MONGO_URL || "";
+    await mongoose.connect(URL);
+    console.log(`Database connected successfully ${URL}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(
+        `Theres been an error while connecting to DB:${error.message}`
+      );
     }
+  }
 }
 
 export default connectDB;

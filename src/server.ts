57adfe10f28cf8872e63f472cdf71242ecd437ca
Lazcano007 <Pedrolazcano@live.com>
2025-04-14 10:18:52 +0200

    Change url for netlify

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const PORT: string | number = process.env.PORT || 3636;
 
 app.use(
   cors({
-    origin: ["http://localhost:3100", "http://localhost:5173, https://clinquant-frangipane-aecd4a.netlify.app"],
+    origin: ["http://localhost:3100", "http://localhost:5173, https://pet-care-for-bestfriends.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
